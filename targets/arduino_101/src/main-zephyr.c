d2b3ec597f1e279d55e73a26ef978d1f4c2a9cca
Sergio Martinez <sergio.martinez.rodriguez@intel.com>
2016-05-24 08:17:56 +0100

    Cleanup

--- targets/arduino_101/src/main-zephyr.c
+++ targets/arduino_101/src/main-zephyr.c
@@ -1,4 +1,4 @@
-/* Copyright 2016 Samsung Electronics Co., Ltd.
+/* Copyright 2016 Intel Corporation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,9 +18,8 @@
 #include <stdlib.h>
 #include <misc/shell.h>
 
-int jerryscript_entry (int argc, char *argv[]);
-int jerryscript_cmd (int argc, char *argv[]);
-int jerryscript_test (int argc, char *argv[]);
+#include "jerry.h"
+#include "jrt/jrt.h"
 
 #if defined(CONFIG_STDOUT_CONSOLE)
 #include <stdio.h>
@@ -30,32 +29,38 @@ int jerryscript_test (int argc, char *argv[]);
 #define PRINT           printk
 #endif
 
-void
-print_version()
-{
-    printf("Version " __DATE__ __TIME__ " ");
-}
-
-void
-print_help()
-{
-    printf("Help\n");
+/**
+ * Jerryscript simple test loop
+ */
+int jerryscript_test () {
+    const char script [] = "for (t=0; t<10; t++) print ('Hi JS! '+(t+1));";
+    jerry_completion_code_t ret_code;
+    ret_code = jerry_run_simple ((jerry_api_char_t *) script, strlen(script), JERRY_FLAG_EMPTY);
+    return ret_code;
 }
 
 static void
-shell_cmd_help(int argc, char* argv[])
+shell_cmd_syntax_help(int argc, char* argv[])
 {
-    print_help();
+    printf("version jerryscript & zephyr versions\n");
 }
 
 static void
 shell_cmd_version(int argc, char* argv[])
 {
-    print_version();
+    printf("JERRYSCRIPT %s\n", jerry_commit_hash);
+    printf("BRANCH %s\n", jerry_branch_name);
+    printf("BUILD  %s\n", jerry_build_date);
 }
 
 static void
-shell_cmd_execute(int argc, char* argv[])
+shell_cmd_test(int argc, char* argv[])
+{
+    jerryscript_test();
+}
+
+static void
+shell_cmd_handler(int argc, char* argv[])
 {
     printf("START %d\n", argc);
     for (int t=0; t<argc; t++) {
@@ -65,35 +70,26 @@ shell_cmd_execute(int argc, char* argv[])
     if (argc<1)
         return;
 
-   jerryscript_cmd (argc, argv);
    printf("END\n");
 }
 
-static void
-shell_cmd_test(int argc, char* argv[])
-{
-    jerryscript_test(argc, argv);
-}
-
-const struct shell_cmd commands[] = { { "syntax", shell_cmd_help },
+const struct shell_cmd commands[] = { { "syntax", shell_cmd_syntax_help },
                                       { "version", shell_cmd_version },
-                                      { "js", shell_cmd_execute },
                                       { "test", shell_cmd_test },
                                       { NULL, NULL } };
 
 void main(void) {
-    shell_init("jerryscript> ", commands);
-    jerryscript_entry(0,NULL);
+    printf("Jerry Compilation " __DATE__ " " __TIME__ "\n");
+    jerry_flag_t flags = JERRY_FLAG_EMPTY;
+    jerry_init (flags);
+    shell_register_app_cmd_handler(shell_cmd_handler);
+    shell_init("js> ", commands);
 }
 
 void __attribute__ ((noreturn)) abort() {
-    while(1) {
-
-    };
+    while(1) { };
 }
 
 void __attribute__ ((noreturn)) exit(int value) {
-    while(1) {
-
-    };
+    while(1) { };
 }
